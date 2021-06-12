ed4af67a1ea0c4fb8f3d8cb4718e56ab56b3160e
dawoon-joo <wnekdns5015@gmail.com>
2021-06-13 05:17:47 +0900

    flickr 리스트,검색,크리에이트팝 추가

--- js/gallery.js
+++ js/gallery.js
@@ -2,9 +2,40 @@
 
 var url = "https://www.flickr.com/services/rest/?method=flickr.interestingness.getList";
 var key = "46caa83347cecb3bd94f4d1ccbbb627e"
-getFlickr(url, key, 7);
+var url_search = "https://www.flickr.com/services/rest/?method=flickr.photos.search";
+var searchBtn = $("#search button");
+var searchInput = $("#search input");
+var targetEl = "#gallery ul";
+var num = 7;
+getFlickr(url,num);
 
-function getFlickr(url, key, num){
+
+
+$("body").on("click", ".list li a",function(e){
+    e.preventDefault();
+    var imgSrc = $(this).attr("href");
+    createPop(imgSrc);
+})
+
+$("body").on("click",".pop span",function(e){
+    e.preventDefault();
+    $(this).parent().fadeOut(500,function(){
+        $(this).remove();
+    });
+});
+
+searchBtn.on("click",function(e){
+    e.preventDefault();
+    var tags = searchInput.val();
+    if(tags == "") alert("검색어를 입력하세요.");
+    getFlickr(url_search, num, tags);
+})
+
+
+
+
+
+function getFlickr(url, num, tags){
     $.ajax({
         url:url,
         dataType:"json",
@@ -12,29 +43,31 @@ function getFlickr(url, key, num){
             api_key :key,
             per_page: num,
             format:"json",
-            nojsoncallback:1
+            nojsoncallback:1,
+            tags: tags
         }
     })
     .success(function(data){
+        $(targetEl).empty();
         var item = data.photos.photo;
         console.log(item);
-
+        
         $(item).each(function(index, data){
             var text = this.title;
             if(!this.title) text = "default text";
 
-            $("#gallery .inner .list")
+            $("#gallery .list")
                 .append(
                     $("<li>")
                         .append(
                             $("<a>")
                                 .attr({
-                                    href:"https://live.staticflickr.com/"+this.server+"/"+this.id+"_"+this.secret+"_b.jpg"
+                                    href:"https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_b.jpg"
                                 })
                                 .append(
                                     $("<img>")
                                     .attr({
-                                        src:"https://live.staticflickr.com/"+this.server+"/"+this.id+"_"+this.secret+"_m.jpg",
+                                        src:"https://live.staticflickr.com/"+data.server+"/"+data.id+"_"+data.secret+"_m.jpg",
                                         onerror:"javascript:this.parentNode.parentNode:style='display:none;'"
                                     })
                                 ),
@@ -48,4 +81,20 @@ function getFlickr(url, key, num){
     .error(function(err){
         console.lor("데이터를 불러오는데 실패했습니다.")
     })
+}
+
+
+function createPop(imgSrc){
+    $("body").append(
+        $("<aside class='pop'>")
+            .append(
+                $("<img>")
+                .attr({src:imgSrc})
+            )
+            .append(
+                $("<span>")
+                .text("close")
+            )
+    )
+    $(".pop").fadeIn();
 }
